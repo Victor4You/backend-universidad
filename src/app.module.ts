b725005453418834038eb0055a901a0944fb3464
Victor4you <you@example.com>
2026-02-09 16:58:05 -0700

    fix: sync database schema true final

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,7 +46,7 @@ import { Comment } from './posts/entities/comment.entity';
             Post,
             Comment,
           ],
-          synchronize: true, // OBLIGATORIO: false en Vercel
+          synchronize: !isProduction, // OBLIGATORIO: false en Vercel
 
           // CONFIGURACIÓN DE CONEXIÓN
           ssl: shouldSSl ? { rejectUnauthorized: false } : false,
